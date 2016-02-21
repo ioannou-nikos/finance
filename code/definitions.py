bef0a621f220dbe0591887748a6cc1d5c507a774
Nikolaos Ioannou <ioannou.nikos@gmail.com>
2016-02-21 22:12:01 +0200

    Some obsolete files have been deleted, some have been merged and a new notebook added "ask.ipynb" so all the analysis would end up in that notebook.

--- code/definitions.py
+++ code/definitions.py
@@ -2,6 +2,8 @@
 
 __author__ = 'urban'
 
+import fileinput #Added for removing blank or empty lines
+
 #Ορισμοί Δομών Δεδομένων απαραίτητων για την ανάλυση
 #Οι τίτλοι των φορέων ως προς τη σημασία τους
 foreis_dict={'071':'PDE',
@@ -49,3 +51,108 @@ enotites = { '00':'EDRA',
             '06':'SERRES',
             '07':'CHALKIDIKI'
             }
+def format_dataframe(indf):
+    '''Τα δεδομένα είναι από τον προϋπολογισμό.
+    Η συνάρτηση παίρνει το αρχείο εσόδων ή εξόδων και δημιουργεί τα παρακάτω νέα πεδία:
+    eidos => logar[:2]
+    enotita => logar[3:5]
+    foreas => logar[6:9]
+    kae => logar[10:14]
+    xiliada => logar[10:11]
+    ekatontada => logar[11:12]
+    '''
+    df = indf.copy()
+    df['eidos'] = df.logar.str[:2]
+    df['enotita'] = df.logar.str[3:5]
+    df['foreas'] = df.logar.str[6:9]
+    df['kae'] = df.logar.str[10:14]
+    df['xiliada'] = df.logar.str[10:11]
+    df['ekatontada'] = df.logar.str[11:12]
+    df['last_three'] = df.logar.str[11:]
+    df['last_two'] = df.logar.str[12:]
+    df['first_two'] = df.logar.str[10:12]
+    df['first_three'] = df.logar.str[10:13]
+    return df
+    
+def build_enotita_year_img(enotita,xronia,dfsrc):
+    ''' Τα δεδομένα που χρησιμοποιώ είναι από τον απολογισμό. 
+    Δημιουργεί ένα αντίγραφο dataframe για την ενότητα και τη χρονιά,
+    υπολογίζοντας παράλληλα τα προοδευτικά αθροίσματα για τα Δεσμευθέντα, Τιμολογηθέντα, 
+    Ενταλθέντα και πληρωθέντα καθώς και το ποσοστό στο οποίο
+    αυτά αντιστοιχούν επί του συνόλου.
+    '''
+    df = dfsrc[dfsrc.source == enotita].copy()
+    df['cs_desm' + str(xronia)] = df['desm' + str(xronia)].cumsum()
+    df['cs_timol' + str(xronia)] = df['timol' + str(xronia)].cumsum()
+    df['cs_ental' + str(xronia)] = df['ental' + str(xronia)].cumsum()
+    df['cs_plir' + str(xronia)] = df['plir' + str(xronia)].cumsum()
+    df['pct_egg_desm' + str(xronia)] = 100 * df['cs_desm'+str(xronia)] / df['egg'+str(xronia)]
+    df['pct_egg_timol' + str(xronia)] = 100 * df['cs_timol'+str(xronia)] / df['egg'+str(xronia)]
+    df['pct_egg_ental' + str(xronia)] = 100 * df['cs_ental'+str(xronia)] / df['egg'+str(xronia)]
+    df['pct_egg_plir' + str(xronia)] = 100 * df['cs_plir'+str(xronia)] / df['egg'+str(xronia)]
+    return df
+    
+def build_dataframe_forea(dfsrc):
+    ''' Τα δεδομένα που χρησιμοποιώ είναι από τον προϋπολογισμό. 
+    Η συνάρτηση παίρνει ένα dataframe εσόδων ή εξόδων δημιουργεί αντίγραφο, 
+    δημιουργεί νέα πεδία το φορέας_ενότητα (foreas_enotita) το φορέα (foreas), 
+    την ενότητα (enotita), εάν είναι έσοδο ('06') ή έξοδο ('02') (eidos)'''
+    df = dfsrc[dfsrc.logar.str.len()==9].copy()
+    df['foreas_enotita'] = df.logar.str[3:] #create foreas_enotita
+    df['foreas'] = df.logar.str[6:] #create foreas
+    df['enotita'] = df.logar.str[3:5] #create enotita
+    df['eidos'] = df.logar.str[:2] #create eidos
+    return df 
+    
+def calculate_diffs_on_year(df):
+    ''' Δεδομένα από προϋπολογισμό
+    Υπολογίζω τα πεδία διαφορών στο δεδομένο ενοποιημένο dataframe.
+    x είναι τα έσοδα
+    '''
+    df['diff_diames_desm'] = df['diamorfomenos_x'] - df['desmefthenta']
+    df['diff_diames_ental'] = df['diamorfomenos_x'] - df['entalthenta']
+    df['diff_diames_plir'] = df['diamorfomenos_x'] - df['plirothenta']
+    
+    df['diff_diamex_desm'] = df['diamorfomenos_y'] - df['desmefthenta']
+    df['diff_diamex_ental'] = df['diamorfomenos_y'] - df['entalthenta']
+    df['diff_diamex_plir'] = df['diamorfomenos_y'] - df['plirothenta']
+    
+    df['diff_eggex_diames'] = df['eggekrimenos_y'] - df['diamorfomenos_x']
+    return df
+    
+def delete_blank_lines(flname):
+    '''Delete all blank lines of a file'''
+    for line in fileinput.FileInput(flname,inplace=1,backup='.bak'):
+        #replace all \r
+        line = line.replace('\r','')
+        line = line.strip()
+        if len(line) > 0:
+            print line
+
+def format_clean_file(in_name,out_name):
+    #open file for reading
+    fin = open(in_name,mode='r')
+    #read all the lines
+    lines = fin.readlines()
+    #open file for writting
+    fout = open(out_name,mode='w')
+    #set the buffer
+    buf = ''
+    #loop through the lines
+    for line in lines:
+        if (line[:4].isdigit()) & (line[4:5]==';'):
+            buf = line
+            fout.write(buf)
+            buf = ''
+            continue
+        line = line.strip()
+        if line[:4].isdigit():
+            if buf=='':
+                buf = (line + ';')
+                print buf
+            else:
+                fout.write(buf + '\n')
+                buf = ''
+        else:
+            buf = buf + line
+    
